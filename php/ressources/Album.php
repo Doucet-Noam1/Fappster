738884c809230e2c7dd5f93c3790d5f376065be3
IRPYC37 <o22201947@info27-20.iut45.univ-orleans.fr>
2024-01-29 16:22:49 +0100

    finalisation des ressources

--- php/ressources/Album.php
+++ php/ressources/Album.php
@@ -4,13 +4,31 @@ class Album extends Sortie{
         parent::__construct($nom,$liste,$date,$cover);
     }
     public function render(){
+        echo '<div class="album">';
         echo '<img src=">'.$this->cover.'" </img>';
         echo "<h2>Album</h2>";
         echo "<h1>". $this->nom ."</h1>";
         for ($i= 0;$i<count($this->liste);$i++){
-            $this->liste[$i].render();
+            $this->liste[$i]->render();
         }
         echo "<p>".$this->date."</p>";
+        echo '</div>';
+    }
+
+    public function getNom(){
+        return $this->nom;
+    }
+    public function getCover(){
+        return $this->cover;
+    }
+    public function getDate(){
+        return $this->date;
+    }
+    public function getListe(){
+        return $this->liste;
+    }
+    public function addMusique($song){
+        $this->liste[] = $song;
     }
 
 }
