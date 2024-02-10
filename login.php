f18d39e963c7f248b481e5053c1817aace523a7c
FOURNIER Cyprien <122486705+IRPYC37@users.noreply.github.com>
2024-02-10 15:53:46 +0100

    login+form
    css fait, reste a faire acceuil

--- login.php
+++ login.php
@@ -5,9 +5,18 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Connexion</title>
+<link rel="stylesheet" href="./Css/unifiedForm.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Protest+Riot&display=swap" rel="stylesheet">
 </head>
 <body>
-<h2>Connexion/Inscription</h2>
+    <nav>
+        <img id='logo' src="./data/images/logo.png" alt="Logo">
+    </nav>
+    <div id="panel">
+        <div>
+        <h2>Connexion</h2>
 <form method="post" action="gestionlogin.php">
 <label for="pseudo">Nom d'utilisateur:</label>
 <input type="text" name="pseudo" required><br>
@@ -18,6 +27,10 @@
 
 <button type="submit">Se connecter</button>
 </form>
+        </div>
+        <div class='separator'></div>
+        <div>
+        <h2>Inscription</h2>
 <form method="post" action="gestionlogin.php">
 <label for="pseudo">Nom d'utilisateur:</label>
 <input type="text" name="pseudo" required><br>
@@ -27,5 +40,8 @@
 <input type = "Hidden" name= "signup" value="true">
 <button type="submit">S'inscrire</button>
 </form>
+        </div>
+
+</div>
 </body>
 </html>
