80adeae9d1c9b4f6f87948ce25907f870bbbe355
Livreur <coulontitouann@gmail.com>
2024-02-13 18:07:45 +0100

    Revert "Revert "profil fonctionnel""

--- profil.php
+++ profil.php
@@ -1,16 +1,26 @@
-<?php 
-session_start();
-$pseudo = $_SESSION['pseudo']
-?>
+<html>
 
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profil</title>
+    <title>Onzeur</title>
+    <link rel="stylesheet" href="css/profil.css">
 </head>
+
 <body>
-    <h1> <?php echo $pseudo?></h1>
+    <?php
+    require 'base.php';
+    use onzeur\Type\BD;
+    ?>
+    <div id="contenu">
+        <?php
+        $user = BD::getUtilisateur($_GET['id']??$_SESSION['pseudo']??"");
+        if (is_null($user)) {
+            echo "<h1 id='error'>Cet utilisateur n'existe pas...</h1>";
+            die();
+        }
+        $user->render();
+        ?>
+    </div>
+    <script src="js/profil.js" type="text/javascript"></script>
 </body>
+
 </html>
