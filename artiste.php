6c22321f07ff04bfa029cdfc71aebbe14edc540d
Livreur <coulontitouann@gmail.com>
2024-02-13 15:41:36 +0100

    fix artiste

--- artiste.php
+++ artiste.php
@@ -1,19 +1,14 @@
-<?php
-require 'Classes/autoloader.php';
-Autoloader::register();
-use onzeur\Type\BD;
-
-?>
 <html>
-
-<head>
-    <title>Onzeur</title>
-    <link rel="stylesheet" href="css/index.css">
-</head>
-
-<body>
-    <?php
+    
+    <head>
+        <title>Onzeur</title>
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+    
+    <body>
+        <?php
     require 'base.php';
+    use onzeur\Type\BD;
     ?>
     <div id="contenu">
         <div id="albums">
